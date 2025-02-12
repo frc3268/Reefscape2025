2c70c18b662874bebe22cd8d675948c40eb4caca
fernir3268 <116040382+fernir3268@users.noreply.github.com>
2025-02-12 11:50:20 -0800

    ):

---
+++ src/main/kotlin/frc/lib/test/SwerveModuleTest.kt
@@ -0,0 +1,48 @@
+package frc.lib.test
+
+import org.junit.Test
+
+class SwerveModuleTest {
+
+ @Test
+  fun getSetPointEntry() {}
+
+ @Test
+  fun getAngleEncoderEntry() {}
+
+ @Test
+  fun getAbsoluteEncoderEntry() {}
+
+ @Test
+  fun getTurnController() {}
+
+ @Test
+  fun getDelta() {}
+
+ @Test
+  fun setDelta() {}
+
+ @Test
+  fun update() {}
+
+ @Test
+  fun resetToAbsolute() {}
+
+ @Test
+  fun getState() {}
+
+ @Test
+  fun getPosition() {}
+
+ @Test
+  fun setDesiredState() {}
+
+ @Test
+  fun stop() {}
+
+ @Test
+  fun getIo() {}
+
+ @Test
+  fun getIndex() {}
+}
