5db9afb54b1ed181f2ef5120cf8f1097d33fc58a
Igbalabanov <101612094+chs000-00@users.noreply.github.com>
2025-02-17 15:57:44 -0800

    issues I guess

---
+++ src/main/kotlin/frc/lib/annotation/Logged.kt
@@ -0,0 +1,6 @@
+package frc.lib.annotation
+
+// Blatantly stolen from https://github.com/Team-8-bit/robot-lib/blob/eeca0adc0aef9ddebdf0f7ffa90fb2f635fd1ba2/annotation/src/main/kotlin/org/team9432/annotation/Logged.kt
+
+@Target(AnnotationTarget.CLASS)
+annotation class Logged
